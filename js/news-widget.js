03d258626f35a187eead41f5426acd7c11421f63
veselov-pv <7262830@gmail.com>
2015-08-21 02:02:19 +0300

    request, start...

--- js/news-widget.js
+++ js/news-widget.js
@@ -1,6 +1,14 @@
 var newsWidget = new function () {
+	var DEFAULT_CONTAINER_SELECTOR = '.news-widget-container';
 
 	var widgetData = {};
+
+	var XMLHttpFactories = [
+		function () {return new XMLHttpRequest()},
+		function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+		function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+		function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+	];
 
 	function addWindowLoadEvent(callback) {
 		if (window.addEventListener) { // W3C standard
@@ -24,6 +32,52 @@ var newsWidget = new function () {
 		return toClass(obj) === '[object Object]';
 	}
 
+	function createXMLHTTPObject() {
+		var xmlhttp = false;
+		for (var i=0;i<XMLHttpFactories.length;i++) {
+			try {
+				xmlhttp = XMLHttpFactories[i]();
+			}
+			catch (e) {
+				continue;
+			}
+			break;
+		}
+		return xmlhttp;
+	}
+
+	function sendRequest(url,callback,postData) {
+		var req = createXMLHTTPObject();
+		if (!req) return;
+		var method = (postData) ? "POST" : "GET";
+		req.open(method, url, true);
+		req.setRequestHeader('User-Agent', 'XMLHTTP/1.0');
+		if (postData)
+			req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		req.onreadystatechange = function () {
+			if (req.readyState != 4) return;
+			if (req.status != 200 && req.status != 304) {
+//          alert('HTTP error ' + req.status);
+				return;
+			}
+			callback(req);
+		};
+		if (req.readyState == 4) return;
+		req.send(postData);
+	}
+
+	function beginUpdateData () {
+		sendRequest()
+	}
+
+	function finishUpdateData () {
+
+	}
+
+	function startUpdating (url, limit, interval) {
+
+	}
+
 	function createWidget(container) {
 		if (!isNode(container)) return;
 		var widgetHTML = document.createElement('div');
@@ -32,17 +86,19 @@ var newsWidget = new function () {
 	}
 
 	function initWidget(container) {
-		container = container || document.querySelector('.news-widget-container');
+		container = container || document.querySelector(DEFAULT_CONTAINER_SELECTOR);
 		createWidget(container);
 	}
 
 	function windowLoadHandler(event, container) {
+		event = event || window.event;
 		initWidget(container);
 	}
 
 	addWindowLoadEvent(windowLoadHandler);
 
 	return {
-		init: initWidget
+		init: initWidget,
+		start:
 	}
 };
