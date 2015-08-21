c06b7ad3acd7206728b80f3312beaef0dc7c3077
veselov-pv <7262830@gmail.com>
2015-08-21 05:01:09 +0300

    response processing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 window.onload = function () {
+	var url = 'http://api.massrelevance.com/reccosxof/matchtrax_hashclash_featured_tweets.json',
+		limit = 5,
+		interval = 4000; // ms
+	// or without auto init by className there should be newsWidget.init(container)
+	newsWidget.start(url,limit, interval);
 
 };
--- js/news-widget.js
+++ js/news-widget.js
@@ -1,13 +1,21 @@
 var newsWidget = new function () {
+	var intervalId;
+
 	var DEFAULT_CONTAINER_SELECTOR = '.news-widget-container';
-
 	var widgetData = {};
-
 	var XMLHttpFactories = [
-		function () {return new XMLHttpRequest()},
-		function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-		function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-		function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+		function () {
+			return new XMLHttpRequest()
+		},
+		function () {
+			return new ActiveXObject("Msxml2.XMLHTTP")
+		},
+		function () {
+			return new ActiveXObject("Msxml3.XMLHTTP")
+		},
+		function () {
+			return new ActiveXObject("Microsoft.XMLHTTP")
+		}
 	];
 
 	function addWindowLoadEvent(callback) {
@@ -34,7 +42,7 @@ var newsWidget = new function () {
 
 	function createXMLHTTPObject() {
 		var xmlhttp = false;
-		for (var i=0;i<XMLHttpFactories.length;i++) {
+		for (var i = 0; i < XMLHttpFactories.length; i++) {
 			try {
 				xmlhttp = XMLHttpFactories[i]();
 			}
@@ -46,18 +54,18 @@ var newsWidget = new function () {
 		return xmlhttp;
 	}
 
-	function sendRequest(url,callback,postData) {
+	function sendRequest(url, callback, postData) {
 		var req = createXMLHTTPObject();
 		if (!req) return;
 		var method = (postData) ? "POST" : "GET";
 		req.open(method, url, true);
-		req.setRequestHeader('User-Agent', 'XMLHTTP/1.0');
+		//req.setRequestHeader('User-Agent', 'XMLHTTP/1.0');
 		if (postData)
 			req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 		req.onreadystatechange = function () {
 			if (req.readyState != 4) return;
 			if (req.status != 200 && req.status != 304) {
-//          alert('HTTP error ' + req.status);
+				//TODO: error handler
 				return;
 			}
 			callback(req);
@@ -66,23 +74,60 @@ var newsWidget = new function () {
 		req.send(postData);
 	}
 
-	function beginUpdateData () {
-		sendRequest()
+	function beginUpdateIteration(url, limit) {
+		var urlWithParams = url + '?limit=' + limit;
+		sendRequest(urlWithParams, finishUpdateIteration);
 	}
 
-	function finishUpdateData () {
+	function finishUpdateIteration(requestObj) {
+		var response = JSON.parse(requestObj.response);
+		var newsTweet = document.createElement('div');
+		newsTweet.className = 'news-tweet';
+		var newsTweetAuthorName = document.createElement('div');
+		newsTweetAuthorName.className = 'news-tweet-author-name';
+		newsTweetAuthorName.innerHTML = response[0].user.name;
+		var newsTweetText = document.createElement('div');
+		newsTweetText.className = 'news-tweet-text';
+		newsTweetText.innerHTML = response[0].text;
 
+		newsTweet.appendChild(newsTweetAuthorName);
+		newsTweet.appendChild(newsTweetText);
+
+		// TODO real model for html
+		widgetData.html.querySelector('.widget-content').appendChild(newsTweet);
 	}
 
-	function startUpdating (url, limit, interval) {
+	function startUpdating(url, limit, interval) {
+		beginUpdateIteration(url, limit);
+		intervalId = setInterval(function () {
+			beginUpdateIteration(url, limit);
+		}, interval);
+	}
 
+	function getWidgetData() {
+		return widgetData;
+	}
+
+	function getNewWidgetHTML () {
+		var widgetHTML = document.createElement('div');
+		widgetHTML.className = 'widget';
+		var widgetHeader = document.createElement('div');
+		widgetHeader.className = 'widget-header';
+		widgetHeader.innerHTML = 'News Widget';
+		var widgetContent = document.createElement('div');
+		widgetContent.className = 'widget-content';
+
+		widgetHTML.appendChild(widgetHeader);
+		widgetHTML.appendChild(widgetContent);
+
+		return widgetHTML;
 	}
 
 	function createWidget(container) {
 		if (!isNode(container)) return;
-		var widgetHTML = document.createElement('div');
-		container.newsWidget = isObject(container.newsWidget) ? container.newsWidget : widgetData;
-		container.appendChild(widgetHTML);
+		widgetData.container = container;
+		widgetData.html = getNewWidgetHTML();
+		container.appendChild(widgetData.html);
 	}
 
 	function initWidget(container) {
@@ -92,6 +137,7 @@ var newsWidget = new function () {
 
 	function windowLoadHandler(event, container) {
 		event = event || window.event;
+		//auto init of widget
 		initWidget(container);
 	}
 
@@ -99,6 +145,7 @@ var newsWidget = new function () {
 
 	return {
 		init: initWidget,
-		start: beginUpdateData
+		start: startUpdating,
+		widgetData: getWidgetData()
 	}
 };
